faef85e74fcd0456a56278f9026034bdfc60a1fb
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-06-16 16:32:02 +0530

    Add GSAP and Lenis for smooth scrolling; restructure layout and pages

---
+++ src/hooks/useLenis.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplierL: 2,
+      infinite: false,
+    });
+
+    // Update ScrollTrigger on scroll
+    lenis.on("scroll", ScrollTrigger.update);
+
+    //use GSAP ticker insted of requestAnimationFramer
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+    })
+
+
+    // Prevent lag from GSAP ticker smoothing
+    gsap.ticker.lagSmoothing(0);
+
+    // Refresh ScrollTrigger after setup
+    ScrollTrigger.refresh();
+
+    return () => {
+      gsap.ticker.remove((time) => {
+        lenis.raf(time * 1000);
+      });
+
+      lenis.destroy();
+      ScrollTrigger.killAll();
+    };
+  }, []);
+}
